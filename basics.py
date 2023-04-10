255470943124cd945ea152fce0f49df2c1e46330
Elia Rühle <elia.ruehle@outlook.de>
2023-04-10 13:12:18 +0200

    some additions

---
+++ basics.py
@@ -0,0 +1,18 @@
+
+# !pip install works for new installations
+import datetime as dt# This script should contain all the basics from the structure.md file.
+
+# The following expression uses an f-String, that means the string is formatted and contains an replacement field in curly brackets {}
+# If an variable is not a string a conversion is necessary, this can be done via:
+# !s - calls str() method, !r - calls repr() method, !a calls ascii() method
+
+world = "World"
+print(f"Hello {world}")
+print(f"Hello {world!s}")
+print(f"Hello {world!r}")
+print(f"Hello {world!a}")
+
+# there are also date specifier:
+today = dt.datetime(year=2023, month=4, day=14)
+print(f" Today is the {today:%B %d, %Y}")
+
--- structure.md
+++ structure.md
@@ -29,6 +29,8 @@
 - Installation
   - unter Mac, Windows, Linux hat es ja eigentlich default
   - gibt es eine Web-IDE auf die wir zurückgreifen können?
+  - vielleicht erwähnen, dass man verschiedene Environments anlegen kann z.B. mit Conda
+  - es gibt auch build tools z.B. poetry -> kurz drauf eingehen, ist ähnlich wie cmake, das kennen vielleicht einige?
 - Voting: "Wie viel Kontakt bisher mit Programmierung?" usw
   - gibt uns Anhaltspunkt, wo wir anfangen
-  - Joshi hatte da eine schnieke Anwendung in TGI, damit war die Übung richtig interaktiv
+  - Joshi hatte da eine schnieke Anwendung in TGI, damit war die Übung richtig interaktiv - vielleicht https://www.mentimeter.com/de-DE/education 
