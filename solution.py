b512c6cae1fe3778b41b35827c8ced601f817f73
Anna Bothin <annibothin@gmail.com>
2023-05-11 08:54:27 +0200

    ideas for object orientation taskpython

--- solution.py
+++ solution.py
@@ -1,24 +1,58 @@
-from pprint import pprint
-dict1 = dict()
-set1 = set()
-list1 = list()
+class Person:
+    def __init__(self, name:str, alter:int, beziehungsstatus:str):
+        self.name = name
+        self.alter = alter
+        self.beziehungsstatus = beziehungsstatus
+        return
+    
+    def geburtstag (self):
+        self.alter += 1
+        
+class Kind(Person):
+    def __init__(self, name:str, alter:int, papa: Person):
+        self.name = name
+        self.alter = alter
+        self.mittagsschlaf = self.check_alter(alter)
+        self.papa = papa
+        return
+    
+    def check_alter(self, alter:int):
+        if alter < 5:
+            return True
+        else: 
+            return False
 
-dict1["SWT"] = "Aßmann"
-dict1["Prog"] = "Fetzer"
-dict1["Mathe"] = "Baumann"
-dict1.update({"Ikt" : "Franz"})
-dict1["hallo"] = "welt"
-print(dict1["SWT"])
+    def geburtstag(self):
+        self.alter+=1
+        self.mittagsschlaf= self.check_alter(self.alter)
 
-prof_list = ["Aßmann", "Goetz"]
+class Auto:
+    def __init__(self,farbe : str, marke : str, baujahr: int, fahrer : Person):
+        self.farbe = farbe
+        self.marke = marke 
+        self.baujahr = baujahr
+        self.fahrer = fahrer
+        return
+    
+    def umlackierung (self, farbe):
+        self.farbe = farbe
+        return
+    
+person1 = Person ("Elia", 21, "ledig" )
 
-dict1["SWT"] = prof_list
+auto1 = Auto("grün", "Mercedes", 1990, person1)
 
-pprint(dict1)
+print(type(auto1.fahrer),auto1.fahrer.name,person1.alter, auto1.farbe)
 
-print(dict1.items())
-print("Keys: " + str(dict1.keys()))
-print("Values: " + str(dict1.values()))
+auto1.umlackierung("blau")
+person1.geburtstag()
 
-print("Keys: ", dict1.keys())
-print(dict1.values(), dict1.keys())
+print(type(auto1.fahrer),auto1.fahrer.name,person1.alter, auto1.farbe)
+
+kind1 = Kind("Mo", 4, person1)
+
+print(kind1.papa.name + "ist der Vater von" + kind1.name)
+print(kind1.name, kind1.mittagsschlaf)
+kind1.geburtstag()
+
+print(kind1.name, kind1.mittagsschlaf)
